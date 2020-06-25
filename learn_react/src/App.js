8e6514b7cd8c670376a9038e166631ddaa3980fc
Tran Hieu <hieu.tran@enouvo.com>
2020-06-25 16:03:58 +0700

    update JSX

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -8,13 +8,19 @@ class App extends Component {
   showInforProduct(product) {
     if (product.status) {
       return <h3>
-                ID:   {product.id}
-                <br />
+        ID:   {product.id}
+        <br />
                   Name: {product.name}
-                <br />
+        <br />
                   Status: {product.status ? "Active" : "Pending"}
-              </h3>
+      </h3>
     }
+  }
+  showInforUser(user, index){
+    return <div key={index}>
+              <h2>Ten: {user.name} </h2>
+              <h2>Tuoi: {user.age}</h2>
+           </div>
   }
 
   render() {
@@ -25,10 +31,32 @@ class App extends Component {
       status: true
     }
 
+    var users = [
+      {
+        id: 1,
+        name: "hieu",
+        age: 20
+      },
+      {
+        id: 2,
+        name: "huyen",
+        age: 30
+      }
+    ];
+    var element1 = users.map((user, index) => {
+      return  this.showInforUser(user, index)
+    });
+
+    var element2 = users.map((user) => {
+      return   <div key={user.id}>
+                  <h1>Ten: {user.name} </h1>
+                  <h1>Tuoi: {user.age}</h1>
+              </div>
+    });
+
     return (
       <div>
         <Header />
-
         <div className="row">
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <Product />
@@ -47,13 +75,12 @@ class App extends Component {
             <br />
             Status: {product.status ? "Active" : "Pending"}
           </h3>
-          
         </div>
-
         <div className="ml-30">
           {this.showInforProduct(product)}
         </div>
-
+        {element1}
+        {element2}
       </div>
     )
   }
