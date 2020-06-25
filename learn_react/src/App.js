d7b24e9cb48bd5432835e0932beff7790146dedc
Tran Hieu <hieu.tran@enouvo.com>
2020-06-25 11:26:35 +0700

    update

--- learn_react/src/App.js
+++ learn_react/src/App.js
@@ -4,7 +4,27 @@ import Header from './components/Header'
 import Product from './components/Product'
 
 class App extends Component {
+
+  showInforProduct(product) {
+    if (product.status) {
+      return <h3>
+                ID:   {product.id}
+                <br />
+                  Name: {product.name}
+                <br />
+                  Status: {product.status ? "Active" : "Pending"}
+              </h3>
+    }
+  }
+
   render() {
+    var a = 5;
+    var product = {
+      id: 1,
+      name: "iphone",
+      status: true
+    }
+
     return (
       <div>
         <Header />
@@ -16,6 +36,22 @@ class App extends Component {
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <Product />
           </div>
+        </div>
+        <div className="ml-30">
+          <h3>
+            a: {a}
+            <br />
+            ID:   {product.id},
+            <br />
+            Name: {product.name}
+            <br />
+            Status: {product.status ? "Active" : "Pending"}
+          </h3>
+          
+        </div>
+
+        <div className="ml-30">
+          {this.showInforProduct(product)}
         </div>
 
       </div>
